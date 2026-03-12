e6eb578b964192237b8ef09c5e2b4dc827af195d
dangherve <dangherve@gmail.com>
2026-03-12 13:51:38 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/KristjanESPERANTO/MMM-OneCallWeather.git
+git fetch upstream
+git rebase upstream/main
+git remote set-url origin git@github.com:dangherve/MMM-OneCallWeather.git
+git push --force --set-upstream origin main
